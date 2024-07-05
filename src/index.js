5eb3686edbad1ac2f52dabee664104c0a18b558d
JacklinPrempeh <JackieP6132@gmail.com>
2024-07-05 10:17:43 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let one = document.getElementById('first');
+one.style.backgroundColor=('purple');
+one.style.color=('white');
+one.style.padding=('20pt');
+let two = document.getElementById('second');
+two.style.backgroundColor=('orange');
+two.style.color=('black');
+two.innerHTML=('I know now what the DOM is👌');
+let three = document.getElementById('third');
+three.style.backgroundColor=('brown');
+three.style.color=('black');
+three.innerHTML=('I know how to access /select elements in the DOM😁');
+let four= document.getElementById('fourth');
+four.style.backgroundColor=('indigo');
+four.style.color=('black');
+four.style.textShadow=('5pt 5pt 5pt white')
+four.innerHTML=('I know how to use the style methods to change the style of an element in the DOM👌👌');
+let five = document.getElementById('fifth');
+five.style.backgroundColor=('grey');
+five.style.color=('black');
+five.innerHTML=('I know how the styling properties differ in JS and CSS😊');
+let six= document.getElementById('sixth');
+six.style.backgroundColor=('red');
+six.style.color=('black');
+six.innerHTML=('Yes I did it!! I manipulated the DOM and styled my elements😍😍😍');
+
+
